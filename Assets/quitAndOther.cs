f2f164286d0d6b202f4e4ffd247c8a2f6831015b
elliott31 <elliott.potiron@gmail.com>
2025-04-29 12:55:01 +0200

    Scene

--- Assets/quitAndOther.cs
+++ Assets/quitAndOther.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class quitAndOther : MonoBehaviour
 {
+    public Vector3 spawnPoint;
+    public GameObject plane;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,5 +23,9 @@ public class quitAndOther : MonoBehaviour
     {
         SceneManager.LoadScene("JeuNiveau1");
 
+    }
+    public void LaunchGame()
+    {
+        plane.transform.position = spawnPoint;
     }
 }
